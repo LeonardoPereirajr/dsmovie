a2af6813a980af6dc4af9c7e54b1e45a6c60b7ef
Leonardo Sanches <leonardopereirajr@gmail.com>
2022-02-15 19:54:48 -0300

    Save Score

---
+++ backend/src/main/java/com/devsuperior/dsmovies/controllers/ScoreController.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dsmovies.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmovies.dto.MovieDTO;
+import com.devsuperior.dsmovies.dto.ScoreDTO;
+import com.devsuperior.dsmovies.service.ScoreService;
+
+@RestController
+@RequestMapping(value = "/scores")
+public class ScoreController {
+	
+	@Autowired
+	private ScoreService service;
+	
+	@PutMapping // salvamento idpontente salvou uma vez e não muda nas proximas.
+	public MovieDTO saveScore(@RequestBody ScoreDTO dto){
+		MovieDTO movieDTO = service.saveScore(dto);
+		return movieDTO;
+	}
+}
